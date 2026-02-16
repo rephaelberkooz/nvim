74913699dc31227137f8799f796588af82129b68
Rephael Berkooz <rephael@berkooz.com>
2026-02-16 12:19:37 -0500

    using new vim.lsp

--- lua/reph/lsp/init.lua
+++ lua/reph/lsp/init.lua
@@ -1,51 +1,52 @@
 local coq = require("coq")
-local lspconfig = require("lspconfig")
-
--- Ensure coq capabilities are properly merged
-local capabilities = coq.lsp_ensure_capabilities()
-
 -- Configure LSP servers with coq
-lspconfig.pyright.setup(coq.lsp_ensure_capabilities({
+-- lspconfig.pyright.setup(coq.lsp_ensure_capabilities({
+--   on_attach = function(client, bufnr)
+--     -- Force LSP to provide completions
+--     client.server_capabilities.completionProvider = {
+--       resolveProvider = true,
+--       triggerCharacters = { ".", "(", "[" }
+--     }
+--   end,
+--   settings = {
+--     python = {
+--       analysis = {
+--         typeCheckingMode = "standard",
+--         extraPaths = { 
+--           "~/Nuclei/nuclei-compliance/nuclei-compliance/shared/"
+--         },
+--         autoSearchPaths = true,
+--         useLibraryCodeForTypes = true,
+--         diagnosticMode = "openFilesOnly", -- is this giving me workspace-level errors
+--         completeFunctionCalls = true,
+--         autoImportCompletions = true,
+--         indexing = true,
+--         inlayHints = {
+--             callArgumentNames = "all"
+--         },
+--       }
+--     }
+--   }
+-- }))
+
+vim.lsp.config('ty', coq.lsp_ensure_capabilities({
+    settings = {
+        ty = {}
+    }
+}))
+vim.lsp.enable('ty')
+
+vim.lsp.config('ts_ls', coq.lsp_ensure_capabilities({
   on_attach = function(client, bufnr)
-    -- Force LSP to provide completions
     client.server_capabilities.completionProvider = {
       resolveProvider = true,
       triggerCharacters = { ".", "(", "[" }
     }
   end,
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "standard",
-        extraPaths = { 
-          "~/Nuclei/nuclei-compliance/nuclei-compliance/shared/"
-        },
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "openFilesOnly", -- is this giving me workspace-level errors
-        completeFunctionCalls = true,
-        autoImportCompletions = true,
-        indexing = true,
-        inlayHints = {
-            callArgumentNames = "all"
-        },
-      }
-    }
-  }
 }))
-
-lspconfig.ts_ls.setup(coq.lsp_ensure_capabilities({
-  on_attach = function(client, bufnr)
-    client.server_capabilities.completionProvider = {
-      resolveProvider = true,
-      triggerCharacters = { ".", "(", "[" }
-    }
-  end,
-}))
-
-lspconfig.eslint.setup(coq.lsp_ensure_capabilities({}))
-
 vim.lsp.enable('ts_ls')
+
+vim.lsp.config('eslint', coq.lsp_ensure_capabilities({}))
 vim.lsp.enable('eslint')
 
 
